c48839cdda3ab0c83e37aac31f5a18f1847041a1
devshum <57689318+devshum@users.noreply.github.com>
2021-08-11 20:48:02 +0300

    the board's head was styled

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,7 +1,14 @@
 import styles from './Board.module.css';
+import BoardHead from './BoardHead/BoardHead';
 
 const Board = () => {
-  return <div className={styles.board}>This is a board</div>
+  return (
+    <div className={styles.board}>
+      <BoardHead />
+      <div>Tasks on hold</div>
+      <div>Tasks completed</div>
+    </div>
+  )
 }
 
 export default Board;

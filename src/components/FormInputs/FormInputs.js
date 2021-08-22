4348bf53a663222df8c408796b3731125f852a2d
devshum <57689318+devshum@users.noreply.github.com>
2021-08-22 20:51:28 +0300

    the data was collected

--- src/components/FormInputs/FormInputs.js
+++ src/components/FormInputs/FormInputs.js
@@ -1,8 +1,64 @@
 import Button from '../Button/Button'
+import { useState } from 'react'
 
-const FormInputs = () => {
+const FormInputs = props => {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredDate: '',
+    enteredStatus: 'pending',
+    enteredImportance: 'minor'
+  })
+
+  const changeTitleHandler = e => {
+    setUserInput(prevState => {
+      return {
+        ...prevState,
+        enteredTitle: e.target.value
+      }
+    })
+  }
+
+  const changeDateHandler = e => {
+    setUserInput(prevState => {
+      return {
+        ...prevState,
+        enteredDate: e.target.value
+      }
+    })
+  }
+
+  const changeStatusHandler = e => {
+    setUserInput(prevState => {
+      return {
+        ...prevState,
+        enteredStatus: e.target.value
+      }
+    })
+  }
+
+  const changeImportanceHandler = e => {
+    setUserInput(prevState => {
+      return {
+        ...prevState,
+        enteredImportance: e.target.value
+      }
+    })
+  }
+
   const submitFormHandler = e => {
     e.preventDefault()
+
+    props.onSaveTaskData({
+      ...userInput,
+      enteredDate: new Date(userInput.enteredDate)
+    })
+
+    setUserInput({
+      enteredTitle: '',
+      enteredDate: '',
+      enteredStatus: 'pending',
+      enteredImportance: 'minor'
+    })
   }
 
   return (
@@ -10,17 +66,17 @@ const FormInputs = () => {
     <form style={{display: 'flex', gap: '30px', alignItems: 'center'}} onSubmit={submitFormHandler}>
       <div>
         <label htmlFor="task">Task Title</label>
-        <input type="text" id="task" />
+        <input type="text" id="task" value={userInput.enteredTitle} onChange={changeTitleHandler}/>
       </div>
 
       <div>
         <label htmlFor="date">Date:</label>
-        <input type="date" id="date" min="2021-01-01" />
+        <input type="date" id="date" min="2021-01-01" value={userInput.enteredDate} onChange={changeDateHandler} />
       </div>
 
       <div>
         <label htmlFor="status">Status:</label>
-        <select id="status">
+        <select id="status" onChange={changeStatusHandler} value={userInput.enteredStatus}>
           <option value="pending">Pending</option>
           <option value="in progress">In progress</option>
         </select>
@@ -28,7 +84,7 @@ const FormInputs = () => {
 
       <div>
         <label htmlFor="importance">Importance:</label>
-        <select id="importance">
+        <select id="importance" onChange={changeImportanceHandler} value={userInput.enteredImportance}>
           <option value="minor">Minor</option>
           <option value="normal">Normal</option>
           <option value="critical">Critical</option>
